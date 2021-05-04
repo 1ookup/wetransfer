a3104c0670ce7328b33ecb75bc421116cf0189e6
as <as@gmail.com>
2021-05-04 21:55:26 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wetransfer',
-    version='0.2',
+    version='0.3',
     packages=["wetransfer"],
     py_modules=['script'],
     install_requires=[
